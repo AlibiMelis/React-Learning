3b243719c236e6873a6fd04e956277429e7027d5
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-01-17 15:15:45 +0800

    Created my first react component'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { render } from '@testing-library/react';
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
       <div className="App">
