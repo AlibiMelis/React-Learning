f5f5a79b714772d531190042c0b376b21c6ee6e5
Alibi Melis <alibimelis@Alibis-MacBook-Pro.local>
2022-01-19 11:40:29 +0800

    Added loader

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({robots: users}));
   }
 
   onSearchChange = (event) => {
@@ -19,10 +25,13 @@ class App extends Component {
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+    });
+    if (filteredRobots.length === 0) {
+      return <h1>Loading</h1>;
+    }
     return (
       <div className='tc'>
-        <h1>Robofriends</h1>
+        <h1 className='f1'>Robofriends</h1>
         <SearchBox onChange={this.onSearchChange} />
         <CardList robots={filteredRobots}/>
       </div>
